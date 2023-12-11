e3874d06b9c59b5545ca1ecdc2c3e1ff82f03707
Fraiters <dutovmv@mail.ru>
2023-12-11 18:55:10 +0300

    Изменил CORS

--- cat_book_backend/settings.py
+++ cat_book_backend/settings.py
@@ -154,5 +154,6 @@ CORS_ORIGIN_WHITELIST = [
     "http://localhost:8080",
     "http://127.0.0.1:8000",
     "http://localhost:4200",
-    "https://cat-book-frontend-production.up.railway.app/",
+    "http://catbookbackend-production.up.railway.app",
+    "https://cat-book-frontend-production.up.railway.app",
 ]
